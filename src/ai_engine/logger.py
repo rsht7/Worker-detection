014ce21b04651237876faa0f6495b83b3371aa2e
Arshit <arshitverma07@gmail.com>
2025-07-09 13:36:46 +0530

    near-miss and image throttle added

--- src/ai_engine/logger.py
+++ src/ai_engine/logger.py
@@ -2,6 +2,9 @@ import json
 import os
 from datetime import datetime
 
+            # "anomaly_id": anomaly_id,
+
+
 class JSONLogger:
     def __init__(self, output_path):
         self.output_path = output_path
@@ -14,10 +17,13 @@ class JSONLogger:
             with open(self.output_path, "w", encoding="utf-8") as f:
                 json.dump([], f, indent=4)
 
+    # def log_detection(self, frame_num, bbox, conf, anomaly, anomaly_id):
     def log_detection(self, frame_num, bbox, conf, anomaly):
+
         entry = {
             "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
             "frame": frame_num,
+            # "anomaly_id": anomaly_id,
             "person": {
                 "x": bbox[0],
                 "y": bbox[1],
